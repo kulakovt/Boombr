20f4e54d4b30146f2a69be4d71f6a024d7fe078b
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2017-12-04 20:45:03 +0300

    Added script to export images (#16)

---
+++ src/ImageUtility.ps1
@@ -0,0 +1,35 @@
+﻿function Export-Entity([string] $entityPath, [string[]] $fileNames)
+{
+    $srcDir = Join-Path $Config.AuditDir $entityPath
+    $dstDir = Join-Path $Config.ArtifactsDir $entityPath
+
+    New-Item -Path $dstDir -ItemType directory -Force
+
+    Get-ChildItem -path $srcDir | ForEach-Object {
+        $dbImageDir = Join-Path $srcDir, $_.Name
+
+        $fileNames | ForEach-Object {
+            Export-File -srcDir $dbImageDir -fileName $_ -dstDir $dstDir
+        }
+    }
+}
+
+function Export-File ([string] $srcDir, [string] $fileName, [string] $dstDir) {
+    $srcFile = Join-Path $srcDir $fileName
+    $dstFile = Join-Path $dstDir $fileName
+
+    Copy-Item -Path $srcFile -Destination $dstFile
+}
+
+function Export-Image()
+{
+    $timer = Start-TimeOperation -Name 'Export images'
+
+    # Export speakers
+    Export-Entity "speakers" @("avatar.jpg", "avatar.small.jpg")
+
+    # Export friends
+    Export-Entity "friends" @("logo.png", "logo.small.png")
+
+    $timer | Stop-TimeOperation
+}
