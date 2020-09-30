4ab740c5d3249829931712e660a28793ac0fd752
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2020-09-30 21:31:23 +0300

    Add image components to Brand Book generator

--- src/BrandBook.ps1
+++ src/BrandBook.ps1
@@ -97,3 +97,117 @@ function Update-BrandBook()
     Update-BrandCommunity -Path $logoPath
 }
 
+class Image
+{
+    [string] $Path
+    [string] $Name
+    [string] $Format
+    [int] $Width
+}
+
+class ImageFamily
+{
+    [string] $Name
+    [Image[]] $Images
+    [Image] $Preview
+}
+
+class Component
+{
+    [string] $Name
+    [string] $RootPath
+    [string] $ReadMePath
+    [ImageFamily[]] $Families
+}
+
+function Get-Image
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [IO.FileInfo]
+        $File
+    )
+
+    process
+    {
+        $image = [Image]::new()
+        $image.Path = $File.FullName
+        $image.Name = $File.Name
+        $image.Format = $File.Extension.Trim('.').ToLowerInvariant()
+        $image.Width = -1
+        if ($File.Name -match '.*-(?<Width>\d+)\.\w+$')
+        {
+            $image.Width = [int] $Matches.Width
+        }
+        if ($image.Format -eq 'svg')
+        {
+            $svg = Select-Xml -Path $image.Path -XPath '/ns:svg' -Namespace @{ ns = 'http://www.w3.org/2000/svg' }
+            if ($svg -and $svg.Node.width)
+            {
+                $width = $svg.Node.width -replace 'px',''
+                $image.Width = [int] $width
+            }
+        }
+
+        $image
+    }
+}
+
+function Get-FamilyName([string] $ImageName)
+{
+    $familyName = [IO.Path]::GetFileNameWithoutExtension($ImageName)
+    if ($familyName -match '(?<BaseName>.*)-\d+$')
+    {
+        $familyName = $Matches.BaseName
+    }
+
+    $familyName
+}
+
+function Get-Family([string] $Path)
+{
+    $imageFormats = @('*.svg', '*.ai', '*.eps', '*.png')
+    Get-ChildItem -Path $Path -Include $imageFormats -Recurse -File |
+    Get-Image |
+    Group-Object -Property { Get-FamilyName -ImageName $_.Name } |
+    ForEach-Object {
+        $group = $_
+
+        $family = [ImageFamily]::new()
+        $family.Name = $group.Name
+        $family.Images = $group.Group
+        $family.Preview = $family.Images | Where-Object { $_.Format -eq 'png' -and $_.Width -eq 200 } | Select-Object -First 1
+        $family
+    }
+}
+
+function Get-Component
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Path
+    )
+
+    process
+    {
+        $component = [Component]::new()
+        $component.Name = Split-Path -Leaf $Path
+        $component.RootPath = Resolve-Path $Path
+        $component.ReadMePath = Join-Path $component.RootPath 'README.md'
+        $component.Families = Get-Family -Path $component.RootPath
+
+        $component
+    }
+}
+
+function Find-AllComponent([string] $Path)
+{
+    Get-ChildItem $Path -Directory |
+    Select-Object -ExpandProperty 'FullName' |
+    Get-Component
+}
